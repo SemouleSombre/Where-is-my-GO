977b8187bafc832be746d6a37f7376eb8fca4efb
Antoine COLLETTE <antoine.collette@orange.fr>
2024-07-22 14:49:38 +0200

    adding comments

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
+# Import the necessary modules
 import os
 import copy
 import json
 import win32api
+import pandas as pd
+import numpy as np
 
+# Create the form of each element
 WALKS = {
-    'Name':"",
-    'Path':"",
-    'Size':None,
-    'Step':0,
-    'Content' : []
+    'Name':"",      # Name of the file or folder
+    'Path':"",      # Path of the file or folder
+    'Size':None,    # Size of the file or folder
+    'Step':0,       # Step of the walk
+    'Content' : []  # all elements in the folder (uniquely for folders)
 }
 
 def GetDrives() -> list:
+    """
+    Get all drives of the computer
+    Returns:
+        list: List of the names of the drives
+    """
     drives = win32api.GetLogicalDriveStrings()
     drives = drives.split('\000')[:-1]
     return drives
 
-def defineSize(list_of_elements):
+def defineSize(list_of_elements)->int:
+    """
+    Define the size of the folder
+
+    Args:
+        list_of_elements (class): List of the elements in the folder
+
+    Returns:
+        int: sum of the size of all elements in the folder
+    """
     size = 0
     for element in list_of_elements:
         size += element['Size']
     return size
 
-def walking(path, step=0):
+def walking(path:str, step:int=0):
+    """_summary_
+
+    Args:
+        path (_type_): _description_
+        step (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        _type_: _description_
+    """
     head = copy.deepcopy(WALKS)
     metadata = []
     head['Name'] = path
@@ -59,6 +86,11 @@ def walking(path, step=0):
     return head
 
 def Main():
+    """
+    Main function of the program
+    Get all drives of the computer and walk through them
+    Save the result in a json file
+    """
     drives = GetDrives()
     if drives != []:
         PC = []
@@ -67,8 +99,6 @@ def Main():
         PC = {'This PC' : PC}
         with open('PC.json', 'w') as f:
             json.dump(PC, f, indent=4)
-
-
 
 if __name__ == "__main__":
     Main()
